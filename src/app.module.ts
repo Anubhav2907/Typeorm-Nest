9dab1aeddbd95bd8078a47b8d0bd3b45b70c245e
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-07-02 12:43:51 +0530

    completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { loadTypeOrmConnectionFromEnv } from './loader';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return loadTypeOrmConnectionFromEnv();
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/loader.ts
+++ src/loader.ts
@@ -1,5 +1,19 @@
 /* eslint-disable prettier/prettier */
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
+import { ConnectionOptionsEnvReader } from 'typeorm/connection/options-reader/ConnectionOptionsEnvReader';
 export const loadEnvironmentVariables = async () => {
   config();
+};
+export const loadTypeOrmConnectionFromEnv = async () => {
+  const envReader = new ConnectionOptionsEnvReader();
+  const envconfig = await envReader.read();
+  const obj = envconfig[0];
+  const connectionOptions: TypeOrmModuleOptions = {
+    ...obj,
+    entities: [`${__dirname}/${process.env.TYPEORM_ENTITIES}`],
+    migrations: [],
+  };
+  console.log(connectionOptions);
+  return obj;
 };
